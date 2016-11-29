2ff6e102a34e2e381b4115ebbc3f9750217400a4
Filipe Guerra <alias.mac@gmail.com>
2016-11-28 21:10:01 -0800

    Add license (MIT)

--- src/__tests__/Resistor.js
+++ src/__tests__/Resistor.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2016 Filipe Guerra
+ * https://github.com/alias-mac/resistor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 import Resistor from '../Resistor';
 
