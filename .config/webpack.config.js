bbdd9be0059aa784b0a39ec8ca552c6a196d11c9
Filipe Guerra <alias.mac@gmail.com>
2020-02-02 01:31:50 -0800

    More upgrades and changes to css build

--- .config/webpack.config.js
+++ .config/webpack.config.js
@@ -49,19 +49,31 @@ const config = {
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
           {
             loader: 'css-loader',
-            query: {
+            options: {
               sourceMap: true,
-              minimize: true,
             },
           },
           {
             loader: 'postcss-loader',
             options: {
               plugins: () => [
-                require('autoprefixer')({ browsers: ['last 2 versions'] }),
+                require('postcss-flexbugs-fixes'),
+                require('postcss-preset-env')({
+                  autoprefixer: {
+                    flexbox: 'no-2009',
+                  },
+                  stage: 3,
+                }),
+                // Adds PostCSS Normalize as the reset css with default options,
+                // so that it honors browserslist config in package.json
+                // which in turn let's users customize the target behavior as per their needs.
+                require('postcss-normalize')(),
               ],
             },
           },
